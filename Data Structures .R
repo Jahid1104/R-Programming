b469353a4f846f859d75fb553bad0405961eb8b6
Md. Jahid Hasan Jone <jahid.1802049@bau.edu.bd>
2024-02-19 23:21:41 +0600

    Create Data Structures .R

---
+++ Data Structures .R
@@ -0,0 +1,45 @@
+####  R Codes by Md Jahid Hasan Jone
+#### Data Structures
+
+###1.	Vector (ordered collection of same data type)
+X = c(1, 3, 5, 7, 8)
+print(X)
+
+
+###2.	List
+Id = c(1, 2, 3, 4)
+Name = c("A", "B", "C", "D")
+number = 4
+List = list(Id, Name, number)
+print(List)
+
+
+###3.	dataframe
+Name = c("A", "B", "C")
+Language = c("Bangla", "English", "Arabic")
+Age = c(20, 25, 15)
+df = data.frame(Name, Language, Age)
+print(df)
+
+
+###4.	Matrix
+A = matrix (
+        c(1, 2, 3, 4, 5, 6, 7, 8, 9), 
+        nrow = 3, ncol = 3, 	 
+        byrow = TRUE		# By default matrices are in column-wise order 
+)
+print(A)
+
+
+###5.	Array
+A = array(
+        c(1, 2, 3, 4, 5, 6, 7, 8),
+        dim = c(2, 2, 2)					 
+)
+print(A)
+
+
+###6.	Factors
+fac = factor(c("Male", "Female", "Male",
+               "Male", "Female", "Male", "Female"))
+print(fac)
